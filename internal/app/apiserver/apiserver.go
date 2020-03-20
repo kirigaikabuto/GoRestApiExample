f58c839a9f5f90ff05a611065479129cf453df13
Your Name <tleugazy98.com>
2020-03-20 22:38:17 +0600

    dadsad

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,45 @@
+package apiserver
+
+import (
+"github.com/sirupsen/logrus"
+"github.com/gorilla/mux"
+	"io"
+	"net/http"
+)
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+func New(config *Config) *APIServer{
+	return &APIServer{
+		config:config,
+		logger:logrus.New(),
+		router:mux.NewRouter(),
+	}
+}
+
+func(s *APIServer) Start() error{
+	if err:=s.configureLogger();err!=nil{
+		return err
+	}
+	s.logger.Info("starting api server")
+	s.configureRouter()
+	return http.ListenAndServe(s.config.BindAddr,s.router)
+}
+func(s *APIServer) configureLogger()error{
+	level,err:=logrus.ParseLevel(s.config.LogLevel)
+	if err!=nil{
+		return err
+	}
+	s.logger.SetLevel(level)
+	return nil
+}
+func(s *APIServer) handleHello() http.HandlerFunc{
+	return func(w http.ResponseWriter,r *http.Request){
+		 io.WriteString(w,"Hello")
+	}
+}
+func(s *APIServer) configureRouter(){
+	s.router.HandleFunc("/hello",s.handleHello())
+}
